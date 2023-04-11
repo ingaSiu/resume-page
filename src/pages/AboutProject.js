220480941564acbdfcd337cc57f44e284cb1df5c
ingaSiu <ingasiud@gmail.com>
2023-04-11 17:33:06 +0300

    project page

--- src/pages/AboutProject.js
+++ src/pages/AboutProject.js
@@ -1,4 +1,19 @@
+import Topbar from '../components/topbar/Topbar.js';
+import { useParams } from 'react-router-dom';
+import { getProject } from '../func/projectsFunc.js';
+
 const AboutProject = () => {
-  return 'bv';
+  const { id } = useParams();
+  const project = getProject(Number(id));
+  return (
+    <div>
+      <Topbar />
+      <div>
+        <img className="project-img" src={require(`../assets/images/${project.imageName}`)} alt="" />
+      </div>
+      <h1>{project.title}</h1>
+      <h3>{project.description}</h3>
+    </div>
+  );
 };
 export default AboutProject;
