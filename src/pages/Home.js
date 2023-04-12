2f2edd81fd824297ec5cb38b049a378822dfa9c8
ingaSiu <ingasiud@gmail.com>
2023-04-12 21:18:01 +0300

    home page with cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,24 @@
 import ProjectCard from '../components/project-card/ProjectCard.js';
 import Topbar from '../components/topbar/Topbar.js';
+import Button from '../components/Button/Button.js';
+
 import { projectData } from '../data/projectData.js';
+import { getData } from '../func/projectsFunc.js';
+import { useNavigate } from 'react-router-dom';
+
 const Home = () => {
+  const data = getData();
+  const navigate = useNavigate();
   return (
     <div>
       <Topbar />
       <h2>My projects</h2>
       <div className="projects-cards">
+        {data.map((item) => (
+          <ProjectCard key={item.id} imageName={item.imageName} title={item.title}>
+            <Button btnText="Click for more..." onClick={() => navigate(`/projects/${item.id}`)} />
+          </ProjectCard>
+        ))}
         <ProjectCard imageName={projectData[0].imageName} title={projectData[0].title} />
       </div>
     </div>
