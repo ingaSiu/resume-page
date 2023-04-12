422cb48b2642235fd6b082ba52d24f4d157f1dec
ingaSiu <ingasiud@gmail.com>
2023-04-12 21:18:22 +0300

    card about project

--- src/components/project-card/ProjectCard.js
+++ src/components/project-card/ProjectCard.js
@@ -1,13 +1,14 @@
 import './ProjectCard.css';
 
-const ProjectCard = ({ imageName, title }) => {
+const ProjectCard = ({ imageName, title, children }) => {
   return (
-    <div>
+    <div className="project-card-wrapper">
       <div>
-        <img src={require(`../../assets/images/${imageName}`)} alt="" />
+        <img className="card-img" src={require(`../../assets/images/${imageName}`)} alt="" />
       </div>
       <div>
         <h2>{title}</h2>
+        {children}
       </div>
     </div>
   );
